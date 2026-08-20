24f6c5b97726b96804058d68d896c72c1708b2f0
angelvarghesetech-hash <angelvarghesetech@gmail.com>
2026-08-20 10:49:39 +0530

    project

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.http import JsonResponse
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
